6314462bbf2aabf04ea4ad7f1145303987c8bd8d
Vanvu396 <Vanvu396@gmail.com>
2022-01-03 18:26:41 +0700

    add file

---
+++ Sensors and Modules/Lora/Core/Src/main.c
@@ -0,0 +1,211 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "spi.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdlib.h>
+#include "Lora.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi1;
+/* USER CODE BEGIN PV */
+LoRa myLoRa;
+uint8_t read_data[128];
+uint8_t send_data[128];
+int RSSI;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_SPI1_Init();
+  /* USER CODE BEGIN 2 */
+// MODULE SETTINGS ----------------------------------------------
+	myLoRa = newLoRa();
+	
+	myLoRa.hSPIx                 = &hspi1;
+	myLoRa.CS_port               = NSS_GPIO_Port;
+	myLoRa.CS_pin                = NSS_Pin;
+	myLoRa.reset_port            = RESET_GPIO_Port;
+	myLoRa.reset_pin             = RESET_Pin;
+	myLoRa.DIO0_port						 = DIO0_GPIO_Port;
+	myLoRa.DIO0_pin							 = DIO0_Pin;
+	
+	myLoRa.frequency             = 433;							  // default = 433 MHz
+	myLoRa.spredingFactor        = SF_7;							// default = SF_7
+	myLoRa.bandWidth			       = BW_125KHz;				  // default = BW_125KHz
+	myLoRa.crcRate				       = CR_4_5;						// default = CR_4_5
+	myLoRa.power					       = POWER_20db;				// default = 20db
+	myLoRa.overCurrentProtection = 120; 							// default = 100 mA
+	myLoRa.preamble				       = 10;		  					// default = 8;
+	
+	LoRa_reset(&myLoRa);
+	LoRa_init(&myLoRa);
+	
+	// START CONTINUOUS RECEIVING -----------------------------------
+	LoRa_startReceiving(&myLoRa);
+	//---------------------------------------------------------------
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+    // SENDING DATA - - - - - - - - - - - - - - - - - - - - - - - - -
+		send_data[0] = 0x3B; // MY ADDRESS
+		for(int i=0; i<26; i++)
+			send_data[i+1] = 48+i;
+		LoRa_transmit(&myLoRa, send_data, 128, 500);
+		HAL_Delay(1500);
+		
+		// RECEIVING DATA - - - - - - - - - - - - - - - - - - - - - - - -
+		LoRa_receive(&myLoRa, read_data, 128);
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
---
+++ Sensors and Modules/Lora/README.md
@@ -0,0 +1,17 @@
+Hardware
+Requirements
+Ra-02 LoRa module with SX1278 chip
+433MHz antenna
+STM32 microcontroller LoRa Ra-02 433MHz antenna
+Wiring
+The wire connections are like following:
+
+STM pin	Module pin
+SPI MISO	MISO
+SPI MOSI	MOSI
+SPI CLK	CLK
+a GPIO output (initially HIGH)	NSS
+a GPIO output (initially HIGH)	RST
+a GPIO input (EXTI - Rising edge)	DIO0
+3.3v	3.3v
+GND	GND
